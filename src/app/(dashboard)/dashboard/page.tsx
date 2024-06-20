5f9a1ddf8b08fdd4da0dc7b2421e05ef67d3883b
Gaurav Bhandari <sbhandari1907@protonmail.com>
2024-06-21 00:01:13 +0530

    create dashboard layout

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+  return (
+    <div className="">
+      <p className="border-b font-heading font-bold  text-2xl py-3 px-2">Weird Title</p>
+    </div>
+  );
+};
+export default Dashboard;
